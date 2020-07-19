ba7011bd649988b413b403536b1df7a71f5bef48
Murilo Gomes <murilogomes526@gmail.com>
2020-07-19 14:36:47 -0300

    Adição de equipamento de defesa e atributos alcance e inteligencia

--- src/br/unicamp/mc322/lab10/projeto/mapObjects/characters/Character.java
+++ src/br/unicamp/mc322/lab10/projeto/mapObjects/characters/Character.java
@@ -9,31 +9,37 @@ import br.unicamp.mc322.lab10.projeto.mapObjects.Sprite;
 import br.unicamp.mc322.lab10.projeto.mapObjects.objects.inventoryItems.CanCarry;
 import br.unicamp.mc322.lab10.projeto.mapObjects.objects.inventoryItems.Money.Money;
 import br.unicamp.mc322.lab10.projeto.mapObjects.objects.inventoryItems.equipment.attack.Attack;
+import br.unicamp.mc322.lab10.projeto.mapObjects.objects.inventoryItems.equipment.defense.Defense;
 
 public abstract class Character extends GameObject{
 	private static final int INVENTORY_MAX_AMOUNT = 30;
 	private int HP;
-	private int ATKValue;
-	private int DEFValue;
+	private int intelligence;
+	private int ATKValue; //quantidade de dados de ataque que o personagem possui no momento, considerando bonus de armas ou magias
+	private int DEFValue; //quantidade de dados de defesa que o personagem possui no momento, considerando bonus de equipamentos ou magias
+	private int range; //alcance do personagem para realizar ataques, para todos os personagens é igual a 1 quando desarmado
 	protected CanCarry[] inventory;		//todos os seres vivos precisam de inventario, pois � onde fica o drop deles que deve cair no chao caso morrerem
 	protected int inventoryLoad = 0;
 	private int MP;		//BodyPoints
-	private int attackDices;
-	private int defenseDices;
-	private Attack[] attackEquipament = new Attack[2];		//equipamento de ataque dos seres vivos ou null caso use as maos/garras/dentes etc
+	private int attackDices; //quantidade de dados de ataque que o personagem possui inicialmente, nunca se altera
+	private int defenseDices; //quantidade de dados de defesa que o personagem possui inicialmente, nunca se altera
+	private Attack[] attackEquipment = new Attack[2];		//equipamento de ataque dos seres vivos ou null caso use as maos/garras/dentes etc
+	private Defense defenseEquipment;
 	private Boolean dead = false;
 	private Money money = new Money();
 	
-	public Character(String name, GameTypeObjects id, int hp, int mp, Sprite sprite, int attackDices, int defenseDices) {
+	public Character(String name, GameTypeObjects id, int hp, int mp, int intelligence, Sprite sprite, int attackDices, int defenseDices) {
 		super(name,sprite,id);
 		this.HP = hp;
 		this.MP = mp;
+		this.intelligence = intelligence;
 		ATKValue = attackDices;
 		DEFValue = defenseDices;
 		inventory = new CanCarry[INVENTORY_MAX_AMOUNT];
+		setRange(1);
 	}
 	
-	public Character(String name, GameTypeObjects id, int hp, int mp, Sprite sprite, int attackDices, int defenseDices, Coordinate position) {
+	public Character(String name, GameTypeObjects id, int hp, int mp, int intelligence, Sprite sprite, int attackDices, int defenseDices, Coordinate position) {
 		super(name,sprite,id,position);
 		this.HP = hp;
 		this.MP = mp;
@@ -51,15 +57,15 @@ public abstract class Character extends GameObject{
 	}
 	
 	public void setAttackValue(int newAttackValue) {
-	/* Obtido o valor de ataque padrao da criatura, soma ele com o bonus do equipamento se existir */
-	
+	/*O valor do ataque ja deve chegar com o acrescimo das armas */
+		ATKValue = newAttackValue;
 	}
 	
 	public int getAttackValue() {
 		return ATKValue;
 	}
 	
-	public void SetDefenseValue(int newDEFValue) {
+	public void setDefenseValue(int newDEFValue) {
 	/* O valor da defesa ja deve chegar tendo seu valor acrescido do bonus da armadura/escudo */
 		DEFValue = newDEFValue;
 	}
@@ -83,6 +89,7 @@ public abstract class Character extends GameObject{
 	public int getDefenseDices() {
 		return defenseDices;
 	}
+	
 	
 	public void takeDamage(int damage) {
 	/* Desconta do HP atual a quantidade dada, se morrer, ativa o dead */
@@ -169,6 +176,38 @@ public abstract class Character extends GameObject{
 		setPosition(new Coordinate(destinoX, destinoY));
 
 	}
+
+	public Attack[] getAttackEquipment() {
+		return attackEquipment;
+	}
+
+	public void setAttackEquipment(int pos, Attack attackEquipment) {
+		if(this.attackEquipment[pos] == null) {
+			this.attackEquipment[pos] = attackEquipment;
+			if(attackEquipment.getReach() > range)
+				setRange(attackEquipment.getReach());
+		}else {
+			//Disparar exceção de que esta mão ja possui equipamento
+		}
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public Defense getDefenseEquipment() {
+		return defenseEquipment;
+	}
+
+	public void setDefenseEquipment(Defense defenseEquipment) {
+		this.defenseEquipment = defenseEquipment;
+	}
+	
+	
 	
 
 }
