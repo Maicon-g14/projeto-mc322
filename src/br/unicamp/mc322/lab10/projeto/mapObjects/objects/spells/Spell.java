caa3ad7735f206bc364c1230397eb797e908bf93
Murilo Gomes <murilogomes526@gmail.com>
2020-07-21 13:01:05 -0300

    pequenos ajustes

--- src/br/unicamp/mc322/lab10/projeto/mapObjects/objects/spells/Spell.java
+++ src/br/unicamp/mc322/lab10/projeto/mapObjects/objects/spells/Spell.java
@@ -8,15 +8,18 @@ package br.unicamp.mc322.lab10.projeto.mapObjects.objects.spells;
 import br.unicamp.mc322.lab10.projeto.mapObjects.GameObject;
 import br.unicamp.mc322.lab10.projeto.mapObjects.GameTypeObjects;
 import br.unicamp.mc322.lab10.projeto.mapObjects.Sprite;
+import br.unicamp.mc322.lab10.projeto.mapObjects.characters.Controller;
 
-public class Spell extends GameObject{	
-	private int dices;
+public abstract class Spell extends GameObject{	
+	private int dices; //Não entendi o propósito desse atributo
 	private SpellElements spellElement;
+	private SpellTypes spellType;
 	
-	public Spell(String name, Sprite sprite, GameTypeObjects id, int dices, SpellElements spellElement) {
+	public Spell(String name, Sprite sprite, GameTypeObjects id, int dices, SpellElements spellElement, SpellTypes spellType) {
 		super(name,sprite,id);
 		this.dices = dices;
 		this.spellElement = spellElement;
+		this.spellType = spellType;
 	}
 	
 	public int getDices() {
@@ -26,5 +29,11 @@ public class Spell extends GameObject{
 	public SpellElements getElement() {
 		return spellElement;
 	}
+	
+	public SpellTypes getSpellType() {
+		return spellType;
+	}
+	
+	public abstract void use(Controller target);
 	
 }
