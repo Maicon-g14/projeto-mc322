c5d217a0a5381c820701df3d7db41468ea140541
Murilo Gomes <murilogomes526@gmail.com>
2020-07-21 12:59:13 -0300

    Correção para herdar a classe SpellCaster

--- src/br/unicamp/mc322/lab10/projeto/mapObjects/characters/heroes/classes/Wizard.java
+++ src/br/unicamp/mc322/lab10/projeto/mapObjects/characters/heroes/classes/Wizard.java
@@ -4,8 +4,9 @@ import br.unicamp.mc322.lab10.projeto.mapObjects.GameTypeObjects;
 import br.unicamp.mc322.lab10.projeto.mapObjects.Sprite;
 import br.unicamp.mc322.lab10.projeto.mapObjects.characters.heroes.Hero;
 import br.unicamp.mc322.lab10.projeto.mapObjects.objects.inventoryItems.CanCarry;
+import br.unicamp.mc322.lab10.projeto.mapObjects.objects.spells.Spell;
 
-public class Wizard extends Hero {
+public class Wizard extends SpellCaster {
 	private static final char SPRITE = 'W';
 	private static final String NAME = "Wizard";
 	private static final GameTypeObjects ID = GameTypeObjects.WIZARD;
@@ -18,8 +19,16 @@ public class Wizard extends Hero {
 		super(NAME,ID,HP,MP,new Sprite(SPRITE),ATTACK_DICES,DEFENSE_DICES);
 	}
 	
+	public Wizard(Spell[] initialSpells) {
+		super(NAME,ID,HP,MP,new Sprite(SPRITE),ATTACK_DICES,DEFENSE_DICES, initialSpells);
+	}
+	
 	public Wizard(CanCarry[] initialEquipment) {
 		super(NAME,ID,HP,MP,new Sprite(SPRITE),ATTACK_DICES,DEFENSE_DICES,initialEquipment);
+	}
+	
+	public Wizard(CanCarry[] initialEquipment, Spell[] initialSpells) {
+		super(NAME,ID,HP,MP,new Sprite(SPRITE),ATTACK_DICES,DEFENSE_DICES,initialEquipment, initialSpells);
 	}
 
 }
