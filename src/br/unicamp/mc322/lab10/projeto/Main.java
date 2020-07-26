5b9fa2190247392d81348a6d06758cff9fb82226
Tiago <tfelicianogomes@gmail.com>
2020-07-26 18:16:57 -0300

    Adicionando thows exception

--- src/br/unicamp/mc322/lab10/projeto/Main.java
+++ src/br/unicamp/mc322/lab10/projeto/Main.java
@@ -9,7 +9,7 @@ import br.unicamp.mc322.lab10.projeto.map.MapMode;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		HeroQuest game = new HeroQuest(GameMode.STANDARD, MapMode.PREDEFINED, PlayableClasses.WIZARD, "Mago implacavel");
 
 		game.startGame();
